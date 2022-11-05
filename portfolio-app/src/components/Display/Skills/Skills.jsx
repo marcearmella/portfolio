8c73c1aaf8f2718bb2be179b05b14a0e78d14851
marcemolo31 <marcemolo31@gmail.com>
2022-11-05 02:36:04 -0300

    advances in Skills component

--- portfolio-app/src/components/Display/Skills/Skills.jsx
+++ portfolio-app/src/components/Display/Skills/Skills.jsx
@@ -1,7 +1,38 @@
+import skills from './data/skills_data.js';
+import background from '../../../media/images/desktop-background.jpg';
+
 function Skills(){
+    const soft = skills.soft;
+    const hard = skills.hard;
     return (
         <div className="skills_container">
-            <h1>Mis habilidades...</h1>
+            <img className='image' src={background} alt="Imagen de fondo" />
+            <div className="title">
+                <h1>Mis Habilidades</h1>
+            </div>
+            <div className="main">
+                <div className="section softSkills">
+                    <div className="skillsTitle">
+                        <h2>Blandas</h2>
+                    </div>
+                    <ul className="skillsList">
+                        {soft.map(e => 
+                            <li key={e.name}>
+                                <img src={e.icon} className='skill-icon' alt="icon"/>
+                                <p>{e.name}</p>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <div className="section hardSkills">
+                    <div className="skillsTitle">
+                        <h2>Duras</h2>
+                    </div>
+                    <div className="skillsList">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ portfolio-app/src/components/Display/Skills/data/skills_data.js
@@ -0,0 +1,39 @@
+import teamwork from '../../../../media/images/teamwork.png';
+import fastlearning from '../../../../media/images/fastlearning.png';
+
+const skills = {
+    soft: [
+        {
+            name: 'Trabajo en Equipo',
+            icon: teamwork
+        },{
+            name: 'Rápido Aprendizaje',
+            icon: fastlearning
+        },{
+            name: 'Sociable',
+            icon: ''
+        },{
+            name: 'Resolución de Problemas',
+            icon: ''
+        },{
+            name: 'Orientación a Objetivos',
+            icon: ''
+        },{
+            name: 'Responsable',
+            icon: ''
+        }
+    ],
+    hard: [
+        {
+            name: '',
+            description: '',
+            icon: ''
+        },{
+            name: '',
+            description: '',
+            icon: ''
+        }
+    ]
+}
+
+export default skills;
