8775171d9497aa7b88bde421cea092223fed477a
marcemolo31 <marcemolo31@gmail.com>
2022-11-08 21:46:58 -0300

    Version 1.0

--- portfolio-app/src/components/Display/Contact/Contact.jsx
+++ portfolio-app/src/components/Display/Contact/Contact.jsx
@@ -3,27 +3,30 @@ import emailjs from "@emailjs/browser";
 import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import env from '../../../env';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faAt, faSignature, faTags } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Contact({handleCloseModal}){
     const refForm = useRef();
 
     const notifySuccess = () => toast.success("✅ Correo enviado!", {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 2000,
         hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
         progress: undefined,
     });
 
     const notifyFailure = () => toast.failure("⚠️ Ups algo salio mal, intentalo de nuevo!", {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 2000,
         hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
         progress: undefined,
     });
 
@@ -39,7 +42,7 @@ function Contact({handleCloseModal}){
                 notifySuccess();
                 setTimeout(() => {
                     window.location.reload(false);
-                }, 5000);
+                }, 2000);
             },
             () => {
                 notifyFailure();
@@ -56,7 +59,7 @@ function Contact({handleCloseModal}){
                 <form className="contact_form" ref={refForm} onSubmit={sendEmail}>
                     <ToastContainer
                         position="top-center"
-                        autoClose={5000}
+                        autoClose={2000}
                         hideProgressBar={false}
                         newestOnTop={false}
                         closeOnClick
@@ -66,20 +69,63 @@ function Contact({handleCloseModal}){
                         pauseOnHover
                     />
                     <div className="inputs">
-                        <input className="input" placeholder="Nombre" type="text" name="name" required />
-                        <input className="input" placeholder="Email" type="email" name="email" required />
-                        <input className="input" placeholder="Asunto" type="text" name="subject" required />
-                        <textarea
-                            className="input"
-                            placeholder="Deja tu mensaje"
-                            name="message"
-                            required
-                        ></textarea>
+                        <div className="input_container">
+                            <div className="input_icon">
+                                <FontAwesomeIcon icon={faSignature} className="icon" />
+                            </div>
+                            <input className="input" placeholder="Nombre" type="text" name="name" required />
+                        </div>
+                        <div className="input_container">
+                            <div className="input_icon">
+                            <FontAwesomeIcon icon={faAt} className="icon" />
+                            </div>
+                            <input className="input" placeholder="Email" type="email" name="email" required />
+                        </div>
+                        <div className="input_container">
+                            <div className="input_icon">
+                                <FontAwesomeIcon icon={faTags} className="icon" />
+                            </div>
+                            <input className="input" placeholder="Asunto" type="text" name="subject" required />
+                        </div>
+                        <div className="input_container">
+                            <textarea
+                                className="textInput"
+                                placeholder="Deja tu mensaje"
+                                name="message"
+                                required
+                            ></textarea>
+                        </div>
                     </div>
                     <input type="submit" value="Enviar" />
                 </form>
+                <div className="contact_links">
+                    <a href="https://github.com/marcearmella" target="_blank">
+                        <div className="link-container">
+                            <h2>GitHub</h2>
+                            <div className="link-icon">
+                                <FontAwesomeIcon icon={faGithub} size="3x" />
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/marcelo-armella/" target="_blank">
+                        <div className="link-container">
+                            <h2>Linkedin</h2>
+                            <div className="link-icon">
+                                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                            </div>
+                        </div>
+                    </a>
+                    <a href="https://www.instagram.com/marcemolo31/" target="_blank">
+                        <div className="link-container">
+                            <h2>Instagram</h2>
+                            <div className="link-icon">
+                                <FontAwesomeIcon icon={faInstagram} size="3x" />
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
-            <button onClick={handleCloseModal}>
+            <button className="closeModal" onClick={handleCloseModal}>
                 CERRAR
             </button>
         </div>
